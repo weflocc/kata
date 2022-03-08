d1e7259670c7f89a6ab724407b873632b6e08504
Emily Vinsen <emilyv@flocc.co>
2022-03-08 16:23:37 +0000

    add hidden option to global slice reference

--- src/partials/globalSliceReference.js
+++ src/partials/globalSliceReference.js
@@ -1,9 +1,10 @@
 const camelCase = require('lodash.camelcase')
 
-const globalSliceReference = (name) => {
+const globalSliceReference = (name, hidden = '') => {
   return {
     name: camelCase(name),
     title: name,
+    hidden: hidden,
     description:
       'This slice is shown on multiple pages. Click the link icon to see/edit it. This will affect all instances.',
     type: 'reference',
