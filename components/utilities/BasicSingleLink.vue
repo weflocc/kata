898cc5b5d622cff0b95bcdf0e0e9753f1cf287b7
Emily Vinsen <emilyv@flocc.co>
2022-03-21 11:55:12 +0000

    added basic single link component for using basicSingleLink in sanity with

---
+++ components/utilities/BasicSingleLink.vue
@@ -0,0 +1,23 @@
+<template>
+  <component
+    :is="getLinkComponent(item)"
+    v-if="item"
+    :to="getSingleLink(item)"
+    :href="getSingleLink(item)"
+    :target="getLinkComponent(item) == 'a' ? '_blank' : '_self'"
+    class="transition-default"
+  >
+    <slot></slot>
+  </component>
+</template>
+
+<script>
+export default {
+  props: {
+    item: {
+      type: Object,
+      default: null,
+    },
+  },
+}
+</script>
