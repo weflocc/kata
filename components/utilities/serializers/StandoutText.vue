aed301465a085492ed66f7cc2b2c3c7231acfa57
Emily Vinsen <emilyv@flocc.co>
2022-03-24 12:04:17 +0000

    standout text update

--- components/utilities/serializers/StandoutText.vue
+++ components/utilities/serializers/StandoutText.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="standout-text slice py-slice-half">
+  <div v-if="text || cite" class="standout-text slice my-large">
     <p v-if="text" v-kata-html="text" class="label-1" />
     <p v-if="cite" v-kata-html="cite" class="para-2 mt-small" />
   </div>
