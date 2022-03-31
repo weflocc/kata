8e7fce9982a853408c87e6410cc221d666e80d8a
HristoFlocc <hristo@flocc.co>
2022-03-31 17:43:23 +0100

    No mb if quote is only child-node

--- components/slices/testimonial/parts/Testimonial04Part.vue
+++ components/slices/testimonial/parts/Testimonial04Part.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="testimonial-4-part">
-    <p v-if="item.quote" v-kata-html="item.quote" class="heading-3 mb-large" />
+    <p v-if="item.quote" v-kata-html="item.quote" class="heading-3" />
     <p v-if="item.name" v-kata-html="item.name" class="label-1" />
     <p v-if="item.job" v-kata-html="item.job" class="para-4 mt-1" />
   </div>
@@ -16,3 +16,11 @@ export default {
   },
 }
 </script>
+
+<style lang="scss">
+.testimonial-4-part {
+  .heading-3 + .label-1 {
+    margin-top: var(--spacing-large);
+  }
+}
+</style>
