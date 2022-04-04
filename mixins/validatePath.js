105450fcba651aa15d86e1032c94d2d782f7512a
Emily Vinsen <emilyv@flocc.co>
2022-04-04 16:23:18 +0100

    validate path update - use sanity api if state not found

--- mixins/validatePath.js
+++ mixins/validatePath.js
@@ -1,11 +1,34 @@
-import { removeBothSlashes } from '../utils/functions'
+import {
+  removeBothSlashes,
+  removeLeadingSlash,
+  removeTrailingSlash,
+} from '../utils/functions'
+import { groq } from '@nuxtjs/sanity'
 
 export default {
-  validate({ route, store, query }) {
+  async validate({ route, store, query, $sanity }) {
     if (query.preview) return true
     const path = route.params
       ? route.params.slug || route.params.pathMatch
       : route.path
-    return store.getters['references/isPathPublished'](removeBothSlashes(path))
+    let clearPath = removeBothSlashes(path)
+    let res = store.getters['references/isPathPublished'](clearPath)
+    clearPath = removeLeadingSlash(clearPath)
+    clearPath = removeTrailingSlash(clearPath)
+
+    let result = store.state.references.index.some((x) => x.slug == clearPath)
+
+    if (res || result) {
+      return true
+    } else {
+      let qroqQuery = groq`*[slug.current == '${clearPath}' && !(_id in path("drafts.**"))]`
+      if ($sanity) {
+        const result = await $sanity.fetch(qroqQuery)
+        return result.length ? true : false
+      } else {
+        console.warn('Validate path: no sanity instance found')
+        return false
+      }
+    }
   },
 }
