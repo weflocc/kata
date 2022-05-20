2063881f0ee731930884d987ead503db4e8705cb
Emily Vinsen <emilyv@flocc.co>
2022-05-20 16:38:59 +0100

    port 3 update

--- components/slices/portfolio/Portfolio03.vue
+++ components/slices/portfolio/Portfolio03.vue
@@ -2,16 +2,14 @@
   <div
     class="slice portfolio-3 mx-auto w-r24/24 md:flex md:justify-between md:space-x-r1/24 md:items-start"
   >
-    <div class="mb-large text-center md:sticky content md:w-r11/24">
-      <h2 v-if="title" v-kata-html="title" class="mb-medium fade-up" />
-      <p v-if="text" v-kata-html="text" class="mb-medium fade-up" />
-      <div v-if="links" class="text-center">
-        <KataLinks :links="links" />
-      </div>
+    <div class="mb-large md:sticky content md:w-r11/24">
+      <h2 v-if="title" v-kata-html="title" class="fade-up" />
+      <p v-if="text" v-kata-html="text" class="mt-medium fade-up" />
+      <KataLinks :links="links" class="mt-medium fade-up" />
     </div>
     <div
       v-if="list && list != null && list.length"
-      class="mb-large grid grid-cols-1 lg:grid-cols-2 gap-medium md:w-r12/24"
+      class="mb-large grid grid-cols-1 lg:grid-cols-2 gap-small md:w-r12/24"
     >
       <div v-for="item in list" :key="item._key" class="fade-up thumbnail">
         <slot name="tease" :item="item">
