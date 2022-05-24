3c45778465856e06555ec743945ec4482a123f4d
Emily Vinsen <emilyv@flocc.co>
2022-05-24 09:51:55 +0100

    hero 04 spacing fix

--- components/slices/hero/Hero04.vue
+++ components/slices/hero/Hero04.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="slice hero-4">
     <div
-      class="w-r24/24 mx-auto md:w-r12/24 md:mx-r6/24 flex flex-col justify-center text-center z-1"
+      class="w-r24/24 mx-auto md:w-r12/24 flex flex-col justify-center text-center z-1"
     >
       <h1 v-kata-html="title" class="heading-1 mb-slice-half" />
     </div>
