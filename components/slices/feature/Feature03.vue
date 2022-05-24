81d563b84e9082292fbb0f1ce1f7a89338092e8a
Emily Vinsen <emilyv@flocc.co>
2022-05-24 15:02:13 +0100

    added reversed option to feature03

--- components/slices/feature/Feature03.vue
+++ components/slices/feature/Feature03.vue
@@ -1,14 +1,23 @@
 <template>
-  <div class="slice feature-3 stack-children">
+  <div
+    class="slice feature-3 stack-children max-h-screen"
+    :class="{
+      reversed: reversed,
+    }"
+  >
     <KataMedia
       :media="media"
       :ratio="ratio"
       :max-width="3000"
       :no-crop="noCrop"
-      class="h-screen w-full object-cover"
+      class="w-full object-cover"
     />
     <div
-      class="w-r24/24 md:w-r16/24 lg:w-r12/24 h-screen flex flex-col justify-end items-start z-1 p-container-margin md:pr-0 content"
+      class="w-r24/24 md:w-r16/24+container-margin lg:w-r12/24+container-margin flex flex-col justify-end items-start z-1 px-container-margin py-slice content"
+      :class="{
+        'ml-auto mr-0': reversed,
+        'mr-auto ml-0': !reversed,
+      }"
     >
       <h2 v-kata-html="title" class="heading-2 text-white mb-medium fade-up" />
       <SanityEmbedContent
@@ -26,6 +35,12 @@ import { title, textBody, links, media, noCrop } from '../shared'
 
 export default {
   mixins: [title, textBody, links, media, noCrop],
+  props: {
+    reversed: {
+      type: Boolean,
+      default: false,
+    },
+  },
   data() {
     return {
       ratio: 16 / 9,
