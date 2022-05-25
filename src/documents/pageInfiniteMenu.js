a3e52afa6fae6772427a1a05fcd095ca2489e5fc
Emily Vinsen <emilyv@flocc.co>
2022-05-25 09:40:30 +0100

    updated inifinite menu

--- src/documents/pageInfiniteMenu.js
+++ src/documents/pageInfiniteMenu.js
@@ -4,14 +4,19 @@ import { RiMenuAddLine } from 'react-icons/ri'
 import { basicSingleLink } from 'part:@weflocc/kata/partials/index'
 
 const linkItem = (index) => {
-  // max of 4 levels deep!
-  if (index < 4) {
+  // max of 5 levels deep!
+  if (index < 5) {
     index++
     return {
       type: 'object',
       title: 'Menu Item',
       name: 'menuItem',
       fields: [
+        {
+          type: 'string',
+          name: 'title',
+          title: 'Custom link text',
+        },
         basicSingleLink,
         {
           title: 'Children',
@@ -20,6 +25,19 @@ const linkItem = (index) => {
           of: [linkItem(index)],
         },
       ],
+      preview: {
+        select: {
+          linkTitle: 'singleLink.internalLink.title',
+          title: 'title',
+        },
+        prepare(selection) {
+          const { title, linkTitle } = selection
+          return {
+            title: title || linkTitle || 'Unknown',
+            media: RiMenuAddLine,
+          }
+        },
+      },
     }
   } else {
     return {
