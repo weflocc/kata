a280350d725c66a6d97def522f7d4f1049da1947
HristoFlocc <hristo@flocc.co>
2022-06-17 14:26:02 +0100

    validatePath will always return true

--- mixins/validatePath.js
+++ mixins/validatePath.js
@@ -7,35 +7,36 @@ import { groq } from '@nuxtjs/sanity'
 
 export default {
   async validate({ route, store, query, $sanity }) {
-    if (query.preview) {
-      console.log('validate preview true')
-      return true
-    } else {
-      const path = route.params
-        ? route.params.slug || route.params.pathMatch
-        : route.path
-      let clearPath = removeBothSlashes(path)
-      let res = store.getters['references/isPathPublished'](clearPath)
-      clearPath = removeLeadingSlash(clearPath)
-      clearPath = removeTrailingSlash(clearPath)
+    return true
+    // if (query.preview) {
+    //   console.log('validate preview true')
+    //   return true
+    // } else {
+    //   const path = route.params
+    //     ? route.params.slug || route.params.pathMatch
+    //     : route.path
+    //   let clearPath = removeBothSlashes(path)
+    //   let res = store.getters['references/isPathPublished'](clearPath)
+    //   clearPath = removeLeadingSlash(clearPath)
+    //   clearPath = removeTrailingSlash(clearPath)
 
-      let result = store.state.references.index.some((x) => x.slug == clearPath)
+    //   let result = store.state.references.index.some((x) => x.slug == clearPath)
 
-      if (res || result) {
-        console.log('validate 1 true')
-        return true
-      } else {
-        let qroqQuery = groq`*[slug.current == '${clearPath}' && !(_id in path("drafts.**"))]`
-        if ($sanity) {
-          const result = await $sanity.fetch(qroqQuery)
+    //   if (res || result) {
+    //     console.log('validate 1 true')
+    //     return true
+    //   } else {
+    //     let qroqQuery = groq`*[slug.current == '${clearPath}' && !(_id in path("drafts.**"))]`
+    //     if ($sanity) {
+    //       const result = await $sanity.fetch(qroqQuery)
 
-          console.log('validate 2', result.length ? true : false)
-          return result.length ? true : false
-        } else {
-          console.warn('Validate path: no sanity instance found')
-          return true
-        }
-      }
-    }
+    //       console.log('validate 2', result.length ? true : false)
+    //       return result.length ? true : false
+    //     } else {
+    //       console.warn('Validate path: no sanity instance found')
+    //       return true
+    //     }
+    //   }
+    // }
   },
 }
