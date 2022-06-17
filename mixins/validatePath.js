e49b8b3d6275f552859b19dca71668c60cbfe857
HristoFlocc <hristo@flocc.co>
2022-06-17 15:13:20 +0100

    Added guards to ensure store state exist before checking path

--- mixins/validatePath.js
+++ mixins/validatePath.js
@@ -7,36 +7,41 @@ import { groq } from '@nuxtjs/sanity'
 
 export default {
   async validate({ route, store, query, $sanity }) {
-    return true
-    // if (query.preview) {
-    //   console.log('validate preview true')
-    //   return true
-    // } else {
-    //   const path = route.params
-    //     ? route.params.slug || route.params.pathMatch
-    //     : route.path
-    //   let clearPath = removeBothSlashes(path)
-    //   let res = store.getters['references/isPathPublished'](clearPath)
-    //   clearPath = removeLeadingSlash(clearPath)
-    //   clearPath = removeTrailingSlash(clearPath)
+    if (query.preview) {
+      console.log('validate preview true')
+      return true
+    } else {
+      const path = route.params
+        ? route.params.slug || route.params.pathMatch
+        : route.path
+      let clearPath = removeBothSlashes(path)
+      clearPath = removeLeadingSlash(clearPath)
+      clearPath = removeTrailingSlash(clearPath)
+      let result = ''
+      let res = ''
 
-    //   let result = store.state.references.index.some((x) => x.slug == clearPath)
+      if (store.state && store.state.references) {
+        res = store.getters['references/isPathPublished'](clearPath)
+        result = store.state.references.index.some((x) => x.slug == clearPath)
+        console.log('res', res)
+        console.log('result', result)
+      }
 
-    //   if (res || result) {
-    //     console.log('validate 1 true')
-    //     return true
-    //   } else {
-    //     let qroqQuery = groq`*[slug.current == '${clearPath}' && !(_id in path("drafts.**"))]`
-    //     if ($sanity) {
-    //       const result = await $sanity.fetch(qroqQuery)
+      if (res || result) {
+        console.log('validate 1 true')
+        return true
+      } else {
+        let qroqQuery = groq`*[slug.current == '${clearPath}' && !(_id in path("drafts.**"))]`
+        if ($sanity) {
+          const result = await $sanity.fetch(qroqQuery)
 
-    //       console.log('validate 2', result.length ? true : false)
-    //       return result.length ? true : false
-    //     } else {
-    //       console.warn('Validate path: no sanity instance found')
-    //       return true
-    //     }
-    //   }
-    // }
+          console.log('validate 2', result.length ? true : false)
+          return result.length ? true : false
+        } else {
+          console.warn('Validate path: no sanity instance found')
+          return false
+        }
+      }
+    }
   },
 }
