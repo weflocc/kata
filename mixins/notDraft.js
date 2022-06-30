572849140d414d7cb5c5f2b5a80a2e399ecb186f
Emily Vinsen <emilyv@flocc.co>
2022-06-30 15:06:19 +0100

    init not draft mixin

---
+++ mixins/notDraft.js
@@ -0,0 +1,20 @@
+// needs work, but an alternative to validate path - error if the page contains draft
+
+export default {
+  data: () => ({ error: true }),
+  created() {
+    if (
+      (this.c && !this.c?._id?.includes('drafts')) ||
+      this.$route.query.preview
+    ) {
+      console.log(this.c?._id)
+      this.error = false
+    }
+
+    if (this.error && this.c != null) {
+      this.c.title = 'Page not found'
+    } else if (this.c == null) {
+      this.c = { title: 'Page not found' }
+    }
+  },
+}
