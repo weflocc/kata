60759e32cb40c197844367fa12ecc3b6ded1a1af
Emily Vinsen <emilyv@flocc.co>
2022-07-25 11:07:11 +0100

    hero 04 update

--- components/slices/hero/Hero04.vue
+++ components/slices/hero/Hero04.vue
@@ -1,16 +1,16 @@
 <template>
   <div class="slice hero-4">
     <div
-      class="w-r24/24 mx-auto md:w-r12/24 flex flex-col justify-center text-center z-1"
+      class="w-r24/24 mx-auto md:w-r12/24 flex flex-col justify-center text-center z-1 mb-large"
     >
-      <h1 class="heading-1 mb-slice-half" v-html="title" />
+      <h1 class="heading-1" v-html="title" />
     </div>
 
-    <div class="w-r24/24 mx-auto text-center z-1 mb-slice">
+    <div class="w-r24/24 mx-auto text-center z-1">
       <KataMedia
         :media="media"
         :no-crop="noCrop"
-        :ratio="3 / 1"
+        :ratio="ratio"
         :max-width="3000"
         class="w-full"
         :show-loader="showLoader"
@@ -25,5 +25,11 @@ import { title, media, noCrop, showLoader } from '../shared'
 
 export default {
   mixins: [title, media, noCrop, showLoader],
+  props: {
+    ratio: {
+      type: Number,
+      default: 3 / 1,
+    },
+  },
 }
 </script>
