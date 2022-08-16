0e3153e7d0feea930f34b3543a0abed5ebe28a78
Emily Vinsen <emilyv@flocc.co>
2022-08-16 16:46:23 +0100

    feature08 update

--- components/slices/feature/Feature08.vue
+++ components/slices/feature/Feature08.vue
@@ -1,10 +1,13 @@
 <template>
   <div class="slice feature-8" :class="{ 'w-r24/24 mx-auto': isContained }">
     <div
-      class="sm:flex mb-large feature-8-inner"
-      :class="{ 'w-r24/24 mx-auto': !isContained }"
+      class="mb-medium feature-8-inner"
+      :class="{
+        'w-r24/24 mx-auto': !isContained,
+        'sm:flex': title && textBody,
+      }"
     >
-      <div :class="{ 'sm:w-r12/24 mr-large': title && textBody }">
+      <div :class="{ 'sm:w-r12/24 mr-r4/24': title && textBody }">
         <h2 v-kata-html="title" class="heading-2 fade-up" />
       </div>
       <div v-if="textBody" :class="{ 'sm:w-r8/24': title && textBody }">
