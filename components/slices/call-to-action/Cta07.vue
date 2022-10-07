23ef66010b24d6907914725e850f4b65d9d8fe91
Emily Vinsen <emilyv@flocc.co>
2022-10-07 14:00:38 +0100

    changed cta07 to be a slider

--- components/slices/call-to-action/Cta07.vue
+++ components/slices/call-to-action/Cta07.vue
@@ -13,15 +13,17 @@
       />
     </div>
 
-    <div
+    <VueSlickCarousel
       v-if="list && list.length > 0"
-      class="fade-up list grid items-center gap-medium"
+      v-bind="settings"
+      ref="cta-7-slider"
+      class="cta-7-slider fade-up list"
     >
       <component
         :is="item.url ? 'a' : 'div'"
         v-for="item in list"
         :key="item._id"
-        class="logo-item"
+        class="logo-item block text-center"
         :title="item.title"
         :aria-label="item.title"
       >
@@ -31,24 +33,62 @@
           :title="item.title"
           width="100"
           height="100"
-          class="w-[100px] h-[100px] object-contain"
+          class="w-[100px] h-[100px] object-contain mx-auto"
         />
       </component>
-    </div>
+    </VueSlickCarousel>
     <KataLinks v-if="links" :links="links" class="mt-medium" />
   </div>
 </template>
 
 <script>
+import VueSlickCarousel from 'vue-slick-carousel'
+import 'vue-slick-carousel/dist/vue-slick-carousel.css'
+// optional style for arrows & dots
+import 'vue-slick-carousel/dist/vue-slick-carousel-theme.css'
 import { title, textBody, links, list } from '../shared'
 export default {
+  components: { VueSlickCarousel },
   mixins: [title, textBody, links, list],
+  props: {
+    settings: {
+      type: Object,
+      default: () => ({
+        arrows: false,
+        dots: true,
+        slidesToShow: 6,
+        centerMode: false,
+        infinite: true,
+        autoplaySpeed: 3000,
+        autoplay: true,
+        responsive: [
+          {
+            breakpoint: 1000,
+            settings: {
+              slidesToShow: 5,
+            },
+          },
+          {
+            breakpoint: 850,
+            settings: {
+              slidesToShow: 4,
+            },
+          },
+          {
+            breakpoint: 700,
+            settings: {
+              slidesToShow: 2,
+              autoplaySpeed: 2000,
+            },
+          },
+        ],
+      }),
+    },
+  },
 }
 </script>
 
 <style lang="scss" scoped>
-.cta-7 .list {
-  grid-template-columns: repeat(auto-fit, minmax(100px, 1fr));
-  place-items: center;
+.cta-7 {
 }
 </style>
