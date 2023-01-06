3f5f6f73c465d7d2a4a23386841875927bd910c9
Emily Vinsen <emilyv@flocc.co>
2023-01-06 14:31:15 +0000

    few search updates

--- components/utilities/KataSearch.vue
+++ components/utilities/KataSearch.vue
@@ -136,12 +136,6 @@ export default {
 </script>
 
 <style lang="scss">
-.search-icon {
-  border: none;
-  width: 20px;
-  height: 20px;
-}
-
 .search-wrap {
   width: 18px;
   height: 18px;
@@ -150,116 +144,122 @@ export default {
   svg path {
     fill: currentColor;
   }
-}
 
-.search-lightbox {
-  @apply flex justify-start items-start fixed w-full h-full left-0 top-0;
-  z-index: 1000;
-  background: rgba($primary, 0.98);
-  transition: 1s ease;
-  padding-top: var(--spacing-slice);
-  overflow-y: scroll;
-  overflow-x: hidden;
-  color: white;
-
-  .close {
-    position: absolute;
-    top: 5%;
-    right: 5%;
+  .search-icon {
+    border: none;
+    width: 20px;
+    height: 20px;
   }
 
-  .text-btn {
-    border-bottom: 2px solid white;
+  .search-lightbox {
+    @apply flex justify-start items-start fixed w-full h-full left-0 top-0;
+    z-index: 1000;
+    background: rgba($primary, 0.98);
+    transition: 1s ease;
+    padding-top: var(--spacing-slice);
+    overflow-y: scroll;
+    overflow-x: hidden;
     color: white;
-    font-weight: bold;
-  }
 
-  .icon-btn {
-    position: absolute;
-    right: 32px;
-    top: 32px;
-    width: 32px;
-    height: 32px;
-
-    &:before,
-    &:after {
+    .close {
       position: absolute;
-      left: 15px;
-      content: ' ';
-      height: 33px;
-      width: 2px;
-      background-color: white;
-    }
-    &:before {
-      transform: rotate(45deg);
-    }
-    &:after {
-      transform: rotate(-45deg);
+      top: 5%;
+      right: 5%;
     }
 
-    @include md-down {
-      right: 15px;
-      top: 0;
-      width: 20px;
-      height: 20px;
+    .text-btn {
+      border-bottom: 2px solid white;
+      color: white;
+      font-weight: bold;
     }
-  }
 
-  .ais-SearchBox-form {
-    position: relative;
-    .ais-SearchBox-input {
-      appearance: none;
-      background-color: white;
-      border: 1px solid white;
-      border-radius: 3px;
-      caret-color: $primary;
-      max-width: 100%;
-      color: black;
-      width: 100%;
-      padding: 5px 10px 5px 40px;
-      outline: none !important;
-    }
-    // &::before {
-    //   content: '';
-    //   background: transparent url('~/assets/svgs/search.svg') no-repeat scroll 0
-    //     0;
-    // }
-    .ais-SearchBox-submit,
-    .ais-SearchBox-reset {
+    .icon-btn {
       position: absolute;
-      top: 0;
-      bottom: 0;
-      margin: auto;
-      width: 20px;
-      height: 20px;
-      outline: none !important;
-      svg {
-        width: 15px;
-        height: 15px;
+      right: 32px;
+      top: 32px;
+      width: 32px;
+      height: 32px;
+
+      &:before,
+      &:after {
+        position: absolute;
+        left: 15px;
+        content: ' ';
+        height: 33px;
+        width: 2px;
+        background-color: white;
+      }
+      &:before {
+        transform: rotate(45deg);
+      }
+      &:after {
+        transform: rotate(-45deg);
+      }
+
+      @include md-down {
+        right: 15px;
+        top: 0;
+        width: 20px;
+        height: 20px;
       }
     }
-    .ais-SearchBox-submit {
-      left: 10px;
-    }
-    .ais-SearchBox-reset {
-      right: 10px;
-      background: white;
-    }
-  }
 
-  .search-result {
-    border-color: currentColor;
-    h3 {
-      transition: 0.5s ease;
+    .ais-SearchBox-form {
+      position: relative;
+      .ais-SearchBox-input {
+        appearance: none;
+        background-color: white;
+        border: 1px solid white;
+        border-radius: 3px;
+        caret-color: $primary;
+        max-width: 100%;
+        color: black;
+        width: 100%;
+        padding: 5px 10px 5px 40px;
+        outline: none !important;
+      }
+      // &::before {
+      //   content: '';
+      //   background: transparent url('~/assets/svgs/search.svg') no-repeat scroll 0
+      //     0;
+      // }
+      .ais-SearchBox-submit,
+      .ais-SearchBox-reset {
+        position: absolute;
+        top: 0;
+        bottom: 0;
+        margin: auto;
+        width: 20px;
+        height: 20px;
+        outline: none !important;
+        svg {
+          width: 15px;
+          height: 15px;
+        }
+      }
+      .ais-SearchBox-submit {
+        left: 10px;
+      }
+      .ais-SearchBox-reset {
+        right: 10px;
+        background: white;
+      }
     }
-    &:hover {
+
+    .search-result {
+      border-color: currentColor;
       h3 {
-        color: $secondary;
+        transition: 0.5s ease;
+      }
+      &:hover {
+        h3 {
+          color: $secondary;
+        }
       }
     }
   }
-}
-mark {
-  background-color: $secondary;
+  mark {
+    background-color: $secondary;
+  }
 }
 </style>
