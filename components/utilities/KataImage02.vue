f637ce74b23d96fe0b3616ddc1b46dfaee991cb6
Emily Vinsen <emilyv@flocc.co>
2023-03-08 09:52:28 +0000

    removed redundant webp from image

--- components/utilities/KataImage02.vue
+++ components/utilities/KataImage02.vue
@@ -32,7 +32,7 @@ export default {
     // https://image.nuxtjs.org/api/options#screens
     sizes: {
       type: String,
-      default: 'xl:100vw (webp)',
+      default: 'xl:100vw',
     },
     lazy: {
       type: Boolean,
