897cca9881a06263b565c61f2327941e9afc0477
Emily Vinsen <emilyv@flocc.co>
2023-05-12 11:48:02 +0100

    updated refresh preview button style

--- components/utilities/KataPreviewRefresh.vue
+++ components/utilities/KataPreviewRefresh.vue
@@ -1,30 +1,23 @@
 <template>
   <client-only>
     <aside
-      class="fixed w-full top-0 bg-gray-300 flex justify-center items-center pointer-events-none"
-      :class="{ 'h-screen': !ready, 'h-5 mb-5': ready }"
-      style="z-index: 3000"
+      class="fixed bottom-0 inset-x-0 mx-auto z-[3000] flex justify-center items-center"
     >
-      <p class="font-semibold mb-0">
-        <span v-if="!ready">Click to start preview</span>
-        <button
-          class="font-semibold underline p-2 pointer-events-all"
-          @click="refresh()"
-        >
-          Refresh
-        </button>
-      </p>
+      <button
+        class="para-1 bg-grey px-medium py-small border-primary transition-default hover:bg-black !font-bold text-white cursor-pointer w-full text-center"
+        @click="refresh()"
+      >
+        {{ ready ? 'Refresh Preview' : 'Loading...' }}
+      </button>
     </aside>
   </client-only>
 </template>
 
 <script>
 export default {
-  data: function () {
-    return {
-      ready: false,
-    }
-  },
+  data: () => ({
+    ready: false,
+  }),
   mounted() {
     this.$nextTick(() => {
       this.refresh()
