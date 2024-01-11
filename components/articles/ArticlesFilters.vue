ac6624cdd368c18e05e7b9fec75e6935bf6cd6c6
Alex Impression <alex@impression.studio>
2024-01-11 19:31:49 +0200

    2.1.265

--- components/articles/ArticlesFilters.vue
+++ components/articles/ArticlesFilters.vue
@@ -70,7 +70,35 @@ export default {
       })
     },
   },
+  mounted() {
+    // Check the hash when the component is mounted
+    this.checkHashAndClick();
+
+    // You can also check the hash on hashchange event
+    window.addEventListener('hashchange', this.checkHashAndClick);
+  },
+
+  beforeDestroy() {
+    // Remove the hashchange event listener to prevent memory leaks
+    window.removeEventListener('hashchange', this.checkHashAndClick);
+  },
   methods: {
+    checkHashAndClick() {
+      // Check if the URL has a specific hash
+      if (window.location.hash === '#advent_and_christmas') {
+        // Trigger the button click
+        this.toggleFilter('448ba0b2-31fd-4f0b-8f04-7e4c47245f38', 'eventCategory');
+      }
+      if (window.location.hash === '#specialist_tours') {
+        this.toggleFilter('8532cffa-a813-4ccb-aed4-264f60017151', 'eventCategory');
+      }
+      if (window.location.hash === '#music') {
+        this.toggleFilter('b5f3d3a6-d136-4885-8626-5b350240e9e0', 'eventCategory');
+      }
+      if (window.location.hash === '#nccl') {
+        this.toggleFilter('f651cdec-cbb9-48dd-9aae-3e9dc5d1d22b', 'eventCategory');
+      }
+    },
     toggleFilter(filterId, filterGroup) {
       this.$store.commit('articles/toggleFilter', {
         filterId: filterId,
